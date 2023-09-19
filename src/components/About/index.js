915c12fdde68e8546d9d1a8200e597a0d7d8ad20
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2023-09-19 09:26:43 +0000

    solution

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+// Write your code here
+import Header from '../Header'
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <Header />
+    <div className="about-img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+        alt="about"
+        className="about-img"
+      />
+    </div>
+  </div>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+// Write your code here
+import './index.css'
+import Header from '../Header'
+
+const Home = () => (
+  <div className="container">
+    <Header />
+    <div className="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+        className="img-home"
+      />
+    </div>
+  </div>
+)
+export default Home
